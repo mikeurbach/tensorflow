48afd28f006b1cc315eab15a870a9f471242e4ad
mikeurbach <mikeurbach@gmail.com>
2020-04-23 12:14:35 -0600

    Register RTL processor pass, and legalize trivial HLO ops to RTL
    * This only represents a subset of HLO ops we can support
    * These are the ones that trivially reduce to RTL ops in Verilog

--- tensorflow/compiler/mlir/rtl/transforms/rtl_process_dataflow.cc
+++ tensorflow/compiler/mlir/rtl/transforms/rtl_process_dataflow.cc
@@ -8,11 +8,43 @@
 #include "mlir/Transforms/DialectConversion.h"  // TF:llvm-project
 #include "tensorflow/compiler/mlir/rtl/ir/rtl_ops.h"
 #include "tensorflow/compiler/mlir/dataflow/ir/dataflow_ops.h"
+#include "tensorflow/compiler/mlir/xla/ir/hlo_ops.h"
 
 namespace mlir {
 namespace rtl {
 
 #include "tensorflow/compiler/mlir/rtl/transforms/rtl_process_dataflow.cc.inc"
+
+class LegalizeDataflow : public PassWrapper<LegalizeDataflow, FunctionPass> {
+  void runOnFunction() override {
+    Operation *op = getFunction();
+
+    MLIRContext *context = op->getContext();
+
+    ConversionTarget target(*context);
+
+    target.addLegalDialect<RTLDialect>();
+
+    OwningRewritePatternList patterns;
+
+    populateWithGenerated(context, &patterns);
+
+    LogicalResult result = applyPartialConversion(op, target, patterns);
+
+    if(failed(result)) {
+      signalPassFailure();
+    }
+  }
+};
+
+static void pipelineBuilder(OpPassManager &passManager) {
+  passManager.addPass(std::make_unique<LegalizeDataflow>());
+}
+
+static PassPipelineRegistration<> pipeline(
+    "rtl-process-dataflow",
+    "Process modules in the Dataflow dialect to generate RTL modules",
+    pipelineBuilder);
 
 } // end namespace rtl
 } // end namespace mlir
