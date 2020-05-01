dead0f8a9cb9a5abf80a17cc274ac1d163a1f44d
mikeurbach <mikeurbach@gmail.com>
2020-05-01 10:56:48 -0600

    Start lifting unit rate ops into functions
    * Inputs for each data channel
    * Inputs for each data valid
    * Outputs for each data ready
    * Outputs for each result channel
    * Outputs for each result valid
    * Inputs for each result ready
    * Wires to hook up the actual computation
    * Logic to compute the valid and ready signals

--- tensorflow/compiler/mlir/rtl/transforms/rtl_process_dataflow.cc
+++ tensorflow/compiler/mlir/rtl/transforms/rtl_process_dataflow.cc
@@ -1,4 +1,8 @@
+#include "llvm/ADT/SmallString.h"  // TF:llvm-project
 #include "llvm/ADT/SmallVector.h"  // TF:llvm-project
+#include "llvm/ADT/Twine.h"  // TF:llvm-project
+#include "llvm/Support/Debug.h"  // TF:llvm-project
+#include "llvm/Support/ScopedPrinter.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"  // TF:llvm-project
 #include "mlir/IR/Builders.h"  // TF:llvm-project
 #include "mlir/IR/OpDefinition.h"  // TF:llvm-project
@@ -9,6 +13,8 @@
 #include "tensorflow/compiler/mlir/rtl/ir/rtl_ops.h"
 #include "tensorflow/compiler/mlir/dataflow/ir/dataflow_ops.h"
 #include "tensorflow/compiler/mlir/xla/ir/hlo_ops.h"
+
+#define DEBUG_TYPE "rtl-process-dataflow"
 
 namespace mlir {
 namespace rtl {
@@ -37,8 +43,144 @@ class LegalizeDataflow : public PassWrapper<LegalizeDataflow, FunctionPass> {
   }
 };
 
+class LiftOpsToFunctions : public PassWrapper<LiftOpsToFunctions, OperationPass<ModuleOp>> {
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+
+    module.walk(
+        [&](FuncOp function) {
+          function.walk(
+              [&](dataflow::UnitRateOp op) {
+                liftUnitRateOp(op, module);
+              });
+        });
+  }
+
+ private:
+  void liftUnitRateOp(dataflow::UnitRateOp op, ModuleOp parentModule) {
+    LLVM_DEBUG(logger.startLine() << "lifting " << op.getOperationName() << "\n");
+
+    Operation &inner = op.body().front().front();
+
+    FunctionType liftedType = liftedFunctionType(inner);
+
+    StringRef liftedName = liftedFunctionName(*op.getOperation());
+
+    FuncOp lifted = FuncOp::create(op.getLoc(), liftedName, liftedType);
+
+    Block *liftedEntry = lifted.addEntryBlock();
+
+    configureLiftedFunctionBlock(inner, liftedEntry);
+
+    parentModule.push_back(lifted.getOperation());
+  }
+
+  FunctionType liftedFunctionType(Operation &op) {
+    Builder builder(op.getContext());
+
+    Type bitType = builder.getI1Type();
+
+    SmallVector<Type, 2> inputTypes;
+    SmallVector<Type, 2> outputTypes;
+
+    for(auto operand : op.getOperands()) {
+      inputTypes.push_back(operand.getType()); // input for the operand data
+      inputTypes.push_back(bitType); // input for the operand valid
+      outputTypes.push_back(bitType); // output for the operand ready
+    }
+
+    for(auto result : op.getResults()) {
+      outputTypes.push_back(result.getType()); // output for the result data
+      outputTypes.push_back(bitType); // output for the result valid
+      inputTypes.push_back(bitType); // input for the result ready
+    }
+
+    return builder.getFunctionType(inputTypes, outputTypes);
+  }
+
+  StringRef liftedFunctionName(Operation &op) {
+    SmallString<10> name;
+    name += op.getName().getStringRef();
+    name += "_";
+    name += std::to_string((long) ((const void *) &op));
+    return name.str();
+  }
+
+  void configureLiftedFunctionBlock(Operation &op, Block *entry) {
+    // we are assuming there is only one result and valid signal for it.
+    // multiple outputs could be acheived with a fork and multiple parallel unit rates.
+    assert(op.getNumResults() == 1 && "unit rate actors only support a single result");
+
+    OpBuilder builder(entry->getParent());
+    Type bitType = builder.getI1Type();
+
+    // wire up data signals to wrapped operation
+    Operation *operation = builder.clone(op);
+    for(unsigned i = 0; i < op.getNumOperands(); ++i) {
+      // index to the data signal in the arguments
+      operation->setOperand(i, entry->getArgument(i * 2));
+    }
+
+    // compute valid signal for results
+    Value valid;
+    switch(op.getNumOperands()) {
+      case 0:
+        {
+          // with no inputs, always valid
+          RankedTensorType attrType = RankedTensorType::get({}, bitType);
+          ConstantOp const1 = builder.create<ConstantOp>(
+              op.getLoc(), bitType, DenseElementsAttr::get(attrType, APInt(1, 1)));
+          valid = const1.getResult();
+          break;
+        }
+      case 1:
+        {
+          // with one input, valid when it is
+          valid = entry->getArgument(1);
+          break;
+        }
+      default:
+        {
+          // with more than one input, valid when they all are
+          SmallVector<Value, 2> operandValids;
+          for(unsigned i = 0; i < op.getNumOperands(); ++i) {
+            // index to the valid signal in the arguments
+            operandValids.push_back(entry->getArgument((i * 2) + 1));
+          }
+          AndOp andValid = builder.create<AndOp>(
+              op.getLoc(), bitType, operandValids, SmallVector<NamedAttribute, 0>());
+          valid = andValid.getResult();
+          break;
+        }
+    }
+
+    // compute ready signal for operands
+    SmallVector<Value, 1> resultsReady;
+    for(unsigned i = entry->getNumArguments() - 1, j = 0; j < op.getNumResults(); --i, ++j) {
+      // index to the ready signals at the end of the arguments
+      resultsReady.push_back(entry->getArgument(i));
+    }
+    resultsReady.push_back(valid);
+    AndOp validAndReady = builder.create<AndOp>(
+        op.getLoc(), bitType, resultsReady, SmallVector<NamedAttribute, 0>());
+
+    // return the ready bits for the inputs, followed by the data and valid bits
+    SmallVector<Value, 3> results;
+    for(unsigned i = 0; i < op.getNumOperands(); ++i) {
+      results.push_back(validAndReady);
+    }
+    results.push_back(operation->getResult(0));
+    results.push_back(valid);
+
+    builder.create<ReturnOp>(op.getLoc(), results);
+  }
+
+  llvm::ScopedPrinter logger{llvm::dbgs()};
+};
+
 static void pipelineBuilder(OpPassManager &passManager) {
   passManager.addPass(std::make_unique<LegalizeDataflow>());
+  passManager.addPass(std::make_unique<LiftOpsToFunctions>());
 }
 
 static PassPipelineRegistration<> pipeline(
