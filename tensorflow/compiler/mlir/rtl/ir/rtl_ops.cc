7aa5e83cb4500948754c98b1d48de8db493aec77
mikeurbach <mikeurbach@gmail.com>
2020-05-23 17:37:53 -0600

    Add hooks for RTL op printing

--- tensorflow/compiler/mlir/rtl/ir/rtl_ops.cc
+++ tensorflow/compiler/mlir/rtl/ir/rtl_ops.cc
@@ -6,9 +6,6 @@ namespace mlir {
 
 namespace rtl {
 
-#define GET_OP_CLASSES
-#include "tensorflow/compiler/mlir/rtl/ir/rtl_ops.cc.inc"
-
 RTLDialect::RTLDialect(MLIRContext *context)
     : Dialect(getDialectNamespace(), context) {
   addOperations<
@@ -33,6 +30,46 @@ LogicalResult ModuleOp::verifyType() {
 
   return success();
 }
+
+static void print(OpAsmPrinter &p, ModuleOp op) {
+}
+static void print(OpAsmPrinter &p, ReturnOp op) {
+}
+static void print(OpAsmPrinter &p, WireOp op) {
+}
+static void print(OpAsmPrinter &p, InstanceOp op) {
+}
+static void print(OpAsmPrinter &p, AddOp op) {
+}
+static void print(OpAsmPrinter &p, AndOp op) {
+}
+static void print(OpAsmPrinter &p, ConstantOp op) {
+}
+static void print(OpAsmPrinter &p, DivOp op) {
+}
+static void print(OpAsmPrinter &p, MulOp op) {
+}
+static void print(OpAsmPrinter &p, NegateOp op) {
+}
+static void print(OpAsmPrinter &p, NotOp op) {
+}
+static void print(OpAsmPrinter &p, OrOp op) {
+}
+static void print(OpAsmPrinter &p, PowOp op) {
+}
+static void print(OpAsmPrinter &p, RemOp op) {
+}
+static void print(OpAsmPrinter &p, ShiftLeftOp op) {
+}
+static void print(OpAsmPrinter &p, ShiftRightOp op) {
+}
+static void print(OpAsmPrinter &p, SubOp op) {
+}
+static void print(OpAsmPrinter &p, XorOp op) {
+}
+
+#define GET_OP_CLASSES
+#include "tensorflow/compiler/mlir/rtl/ir/rtl_ops.cc.inc"
 
 } // end namespace rtl
 } // end namespace mlir
--- tensorflow/compiler/mlir/rtl/ir/rtl_ops.h
+++ tensorflow/compiler/mlir/rtl/ir/rtl_ops.h
@@ -5,6 +5,7 @@
 #include "mlir/IR/Dialect.h"  // TF:llvm-project
 #include "mlir/IR/Function.h"  // TF:llvm-project
 #include "mlir/IR/OpDefinition.h"  // TF:llvm-project
+#include "mlir/IR/OpImplementation.h"  // TF:llvm-project
 #include "mlir/IR/StandardTypes.h"  // TF:llvm-project
 #include "mlir/Interfaces/SideEffects.h"  // from @llvm-project
 #include "tensorflow/compiler/mlir/rtl/ir/rtl_ops.h"
